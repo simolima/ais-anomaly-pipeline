5e6948df57038868c7e7e100a3196413271d1e38
Simone Lima <simon@MacBook-Pro.local>
2026-05-30 01:56:04 +0200

    actions

---
+++ dbt/macros/generate_schema_name.sql
@@ -0,0 +1,9 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- elif target.name == 'prod' -%}
+        {{ custom_schema_name | trim }}
+    {%- else -%}
+        {{ target.schema }}_{{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
